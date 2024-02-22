e076b48a6979a381407594d62a4259e7b6ebed11
Sagar <sagar@Sagars-Mac-mini.local>
2024-02-22 21:54:50 +0600

    initial commit

---
+++ Movie-List-IOS-Test/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  Movie-List-IOS-Test
+//
+//  Created by Sagar on 2/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
